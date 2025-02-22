c0461b4383f3aa675cf206d0355ac9a11c9d6ddc
차민재 <cmjj0824@naver.com>
2025-02-22 16:26:59 +0900

    Chore : driver 모듈의 다양한 기능 사용해보기

--- basics/demoBrowser.py
+++ basics/demoBrowser.py
@@ -4,7 +4,13 @@ from selenium.webdriver.chrome.service import Service
 # 크롬 실행 - webdriver.Chrome() - 기본값
 # 원래 service 인자가 필요하나, chromewebdriver 설치를 해주어 필요없게 만듬 - brew install chromewebdriver
 driver = webdriver.Chrome()
+
+driver.maximize_window() # 창 최대화
 driver.get("http://www.rahulshettyacademy.com")
 print(driver.title) # HTML title
 print(driver.current_url) # 현재 URL
+driver.get("http://www.rahulshettyacademy.com/seleniumPractise/#/")
+driver.back() # 뒤로가기
+driver.refresh() # 새로고침
+driver.forward() # 앞으로가기
 driver.close()
