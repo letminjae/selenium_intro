09b28b54804f7825d86ca8a55d3fc8d12507dd26
차민재 <cmjj0824@naver.com>
2025-02-22 15:35:52 +0900

    Chore : 예외처리 코드 중 try-except-finally 구문 업데이트

--- basics/ExceptionPython.py
+++ basics/ExceptionPython.py
@@ -1,3 +1,7 @@
+# 파이썬 예외처리에 대한 기본 코드 공부
+
+# raise Exception("Error Message") - 예외 발생시키기
+# assert(조건) - 가정설정문, 조건을 걸어 예외가 발생하면 에러 출력
 ItemsInCart = 0
 
 def ExceptionItemInCart(item):
@@ -8,5 +12,16 @@ def ExceptionItemInCart(item):
     print(item)
     pass
   
-ExceptionItemInCart(ItemsInCart) # Exception : Item in cart should be less than 1 - 강제로 예외 발생 처리
-assert(ItemsInCart == 1) # AssertionError - 가정설정문, 조건을 걸어 예외가 발생하면 에러 출력
+# ExceptionItemInCart(ItemsInCart) # Exception : Item in cart should be less than 1 - 강제로 예외 발생 처리
+# assert(ItemsInCart == 1) # AssertionError - 가정설정문, 조건을 걸어 예외가 발생하면 에러 출력
+
+# ====================================================================================================
+# try, except, finally
+try:
+  print("try block")
+  with open("filelog.txt", "r") as reader:
+    reader.read()
+except: # 파이썬에서는 catch가 아닌 except로 사용
+  print("Error : Some how I reached this block because there is a failure in try block")
+finally:
+  print("finally block")
