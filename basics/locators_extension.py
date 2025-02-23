4eec31e3bb5891fb97c55c64e02b2149e8e106ec
차민재 <cmjj0824@naver.com>
2025-02-23 16:11:53 +0900

    Chore : 새 비밀번호 입력 페이지 동작 테스트 코드 작성

--- basics/locators_extension.py
+++ basics/locators_extension.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import time
 
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized") # 크롬창 최대
@@ -14,5 +15,14 @@ password = "qwer1234!" #plain password
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://rahulshettyacademy.com/client/")
 
+# 비밀번호를 잊었어요 클릭
 driver.find_element(By.LINK_TEXT, "Forgot password?").click()
+
+time.sleep(2)
+
+driver.find_element(By.CSS_SELECTOR, "form div:first-child input").send_keys(email)
+driver.find_element(By.CSS_SELECTOR, "form div:nth-child(2) input").send_keys(password)
+driver.find_element(By.CSS_SELECTOR, "form div:nth-child(3) input").send_keys(password)
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
 
