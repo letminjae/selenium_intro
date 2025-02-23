2127d95bf9d8d3cf440c3f1fbc2001ea47287ba1
차민재 <cmjj0824@naver.com>
2025-02-23 16:40:05 +0900

    ADD : find_dropdown_elements.py - 자동 드롭다운의 경우 어떻게 요소를 찾아 동작시키는지 학습

---
+++ basics/find_dropdown_elements.py
@@ -0,0 +1,13 @@
+# 만약 select 요소가 아닌 자동 드롭다운의 경우에는 어떻게 찾아야하는지 학습
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized") # 크롬창 최대
+chrome_options.add_experimental_option("detach", True) # 크롬창이 바로 꺼지는 현상을 방지
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
