24b392665947e5fb5fcb0cfe6ea96de0fb76c5d9
차민재 <cmjj0824@naver.com>
2025-02-23 16:51:09 +0900

    Chore : 자동 드롭다운 설정 후 반복문으로 원하는 요소 선택

--- basics/find_dropdown_elements.py
+++ basics/find_dropdown_elements.py
@@ -11,3 +11,13 @@ chrome_options.add_experimental_option("detach", True) # 크롬창이 바로 꺼
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+
+driver.find_element(By.ID, "autosuggest").send_keys("ind")
+time.sleep(2)
+# 자동 드롭다운의 경우에는 li 태그의 class 이름을 찾아서 a 태그를 찾아야함
+countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")
+print(len(countries))
+for country in countries:
+    if country.text == "India":
+        country.click()
+        break
