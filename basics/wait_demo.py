9d6221c31cda25ad7f7c7b5450974ebf6f829054
차민재 <cmjj0824@naver.com>
2025-02-26 20:43:10 +0900

    Chore : 명시적대기 - WebDriverWait 학습

--- basics/wait_demo.py
+++ basics/wait_demo.py
@@ -3,6 +3,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import time
 
 chrome_options = Options()
@@ -12,11 +14,11 @@ chrome_options.add_experimental_option("detach", True) # 크롬창이 바로 꺼
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 
-driver.implicitly_wait(5) # 암시적 대기 - 전체 페이지 로딩 시간을 기다림
+driver.implicitly_wait(2) # 암시적 대기 - 전체 페이지 로딩 시간을 기다림
 
 # 검색
 driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
-time.sleep(2) # 명시적 대기 - n초 동안 명시적으로 설정된 시간만큼 기다림
+time.sleep(2)
 results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
 print(len(results))
 
@@ -28,6 +30,9 @@ driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
 driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
-time.sleep(2)
+
+wait = WebDriverWait(driver, 10) # 명시적 대기 - n초 동안 명시적으로 설정된 시간만큼 기다림
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".promoInfo"))) # 페이지에 expected_conditions가 나타날 때까지 기다림
+
 print(driver.find_element(By.CLASS_NAME, "promoInfo").text)
 driver.find_element(By.XPATH, "//button[text()='Place Order']").click()
