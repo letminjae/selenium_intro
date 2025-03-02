58884cff1e32dd3496b3c0189748ffbf2464c06b
차민재 <cmjj0824@naver.com>
2025-03-02 17:24:18 +0900

    Chore : 자유롭게 부모 자식창 컨트롤하며 UI 테스팅

--- basics/child_window.py
+++ basics/child_window.py
@@ -12,4 +12,13 @@ chrome_options.add_experimental_option("detach", True) # 크롬창이 바로 꺼
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://the-internet.herokuapp.com/windows")
 
-driver.find_element(By.LINK_TEXT, "Click Here").click()
+driver.find_element(By.LINK_TEXT, "Click Here").click() # 자식창으로 이동
+
+# driver.switch_to.window 를 사용한다.
+windows_opened = driver.window_handles # 현재 열린 창들의 리스트. 0번째는 부모창, 1번째는 자식창
+driver.switch_to.window(windows_opened[1]) # 자식창으로 switch!
+print(driver.find_element(By.TAG_NAME, "h3").text)
+driver.close() # 자식창 닫기
+driver.switch_to.window(windows_opened[0]) # 부모창으로 switch!
+# 부모창에서 Open a new window 확인
+assert "Opening a new window" in driver.find_element(By.TAG_NAME, "h3").text
