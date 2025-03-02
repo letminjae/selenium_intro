6648b8e4331c1bc72babfd063b832bea1ffce90a
차민재 <cmjj0824@naver.com>
2025-03-02 21:11:33 +0900

    Chore : 국가입력, 약관동의, 구매완료 테스트 확인

--- e2e-testing/angular_practice_e2e.py
+++ e2e-testing/angular_practice_e2e.py
@@ -1,10 +1,12 @@
 # angularpractice 페이지 E2E 테스트
 
-# Shop 진입 > 블랙베리 상품을 추가 > Checkout으로 이동 > 
+# Shop 진입 > 블랙베리 상품을 추가 > Checkout으로 이동 > 국가 입력 > 약관 동의 > 구매 완료
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import time
 
 chrome_options = Options()
@@ -26,8 +28,28 @@ for card in cards:
     card.find_element(By.XPATH, "div/button").click()
     break
 
-# Checkout으로 이동
+# Checkout 클릭
 driver.find_element(By.CSS_SELECTOR, "a[class*='nav-link btn btn-primary']").click() # a[class*='nav-link btn btn-primary'] : nav-link, btn, btn-primary가 포함된 a태그
 # driver.find_element(By.XPATH, "//div[@id='navbarResponsive']/ul/li/a").click()
+
+# Checkout 페이지 이동
+driver.find_element(By.CSS_SELECTOR, "button[class*='btn btn-success']").click()
+
+# Country 입력
+driver.find_element(By.ID, "country").send_keys("ind")
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "India")))
+driver.find_element(By.LINK_TEXT, "India").click()
+
+# 약관 동의
+driver.find_element(By.CSS_SELECTOR, "div[class*='checkbox-primary']").click()
+
+# 구매 완료
+driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+
+# 완료 박스 확인
+success_text = driver.find_element(By.CLASS_NAME, "alert-success").text
+print(success_text)
+assert "Success!" in success_text
 
 
