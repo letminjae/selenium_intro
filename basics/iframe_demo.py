720b73a8bb2f2175b3b596388a0c54e4f16d2cdf
차민재 <cmjj0824@naver.com>
2025-03-02 18:03:50 +0900

    Chore : iframe 컨트롤 및 JS로 막아놓은 텍스트 풀기

--- basics/iframe_demo.py
+++ basics/iframe_demo.py
@@ -13,3 +13,19 @@ chrome_options.add_experimental_option("detach", True) # 크롬창이 바로 꺼
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://the-internet.herokuapp.com/iframe")
+
+# Alert 삭제
+driver.find_element(By.CSS_SELECTOR, ".tox-notification__dismiss.tox-button.tox-button--naked.tox-button--icon").click()
+
+# iframe 으로 switch - frame 입력 후, frame ID 파라미터에 입력
+driver.switch_to.frame("mce_0_ifr")
+
+# 사용자가 iframe text를 막아놔서 수정이 안되는 경우, contenteditable 속성을 true로 변경
+driver.execute_script("arguments[0].setAttribute('contenteditable', 'true');", driver.find_element(By.ID, "tinymce"))
+
+driver.find_element(By.ID,"tinymce").clear()
+driver.find_element(By.ID, "tinymce").send_keys("Hello World")
+
+# 다시 원래 페이지로 돌아가기
+driver.switch_to.default_content()
+print(driver.find_element(By.TAG_NAME, "h3").text) # An iFrame containing the TinyMCE WYSIWYG Editor
