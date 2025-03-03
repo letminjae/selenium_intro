d80eebc4ce2810f6758f2be00f1ee0abbc30e9c9
차민재 <cmjj0824@naver.com>
2025-03-03 18:02:46 +0900

    Chore : 테스트 그룹화 및 테스트 Skip 방법

--- pytest/pytest_demo/test_demo2.py
+++ pytest/pytest_demo/test_demo2.py
@@ -1,5 +1,14 @@
 # 두번째 테스트 파일
+# 그룹화 : 그룹화는 @ 키워드를 사용한다.
+import pytest
 
+@pytest.mark.smoke
+@pytest.mark.skip # 수정 중인 TC가 있으니 제외 시키는 방법 - test_demo2_1만 제외!
 def test_demo2_1():
     msg = "Hello"
     assert msg == "Hi", "Test failed because strings do not match"
+
+def test_CreditCard_2():
+    a = 4
+    b = 6
+    assert a+2 == 6, "Addition do not match"
