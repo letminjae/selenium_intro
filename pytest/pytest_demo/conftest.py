43df747415dc8c1e0e7cd459dae1ed3b51fb0fd1
차민재 <cmjj0824@naver.com>
2025-03-04 21:44:21 +0900

    ADD : conftest.py - 테스트 파일이 공통적으로 사용하는 fixture 파일

---
+++ pytest/pytest_demo/conftest.py
@@ -0,0 +1,9 @@
+# conftest!! 테스트 파일들이 공통적으로 사용하는 fixture를 정의하는 파일
+# 테스트 케이스가 여러개면? setup을 하나하나 넣어주지 말고 클래스로 정의해서 사용하자.
+import pytest
+
+@pytest.fixture()
+def setup():
+    print("픽스쳐 셋업..")
+    yield
+    print("저는 yield 때문에 마지막에 실행돼요")
--- pytest/pytest_demo/test_fixture_demo.py
+++ pytest/pytest_demo/test_fixture_demo.py
@@ -1,11 +1,23 @@
 # 픽스쳐란 프리컨디션처럼 테스트 셋업을 해야하는 조건 설정에 대한 부분이다. == 애너테이션
 import pytest
 
-@pytest.fixture()
-def setup():
-    print("픽스쳐 셋업..")
-    yield
-    print("저는 yield 때문에 마지막에 실행돼요")
+# @pytest.fixture()
+# def init():
+#     print("픽스쳐 셋업..")
+#     yield
+#     print("저는 yield 때문에 마지막에 실행돼요")
 
-def test_fixtureDemo(setup): # 파라미터에 fixture 설정한 setup 함수를 넣어준다.
-    print("픽스쳐 실행")
+# def test_fixtureDemo(init): # 파라미터에 fixture 설정한 setup 함수를 넣어준다.
+#     print("픽스쳐 실행")
+
+@pytest.mark.usefixtures("setup") # usefixtures 사용 시 클래스 self에 픽스쳐가 자동으로 입력된다.
+class TestExample:
+
+    def test_fixtureDemo1(self):
+        print("픽스쳐 실행 1")
+
+    def test_fixtureDemo2(self):
+        print("픽스쳐 실행 2")
+
+    def test_fixtureDemo3(self):
+        print("픽스쳐 실행 3")
