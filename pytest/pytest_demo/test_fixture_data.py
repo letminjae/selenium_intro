09e5c5fb952fd4f9a6ee789fdb5ba0f5d20063fa
차민재 <cmjj0824@naver.com>
2025-03-05 20:37:12 +0900

    Chore : test_fixture_data 로깅 성공

--- pytest/pytest_demo/test_fixture_data.py
+++ pytest/pytest_demo/test_fixture_data.py
@@ -1,8 +1,12 @@
 import pytest
+from pytest_demo.BaseClass import BaseClass
 
 @pytest.mark.usefixtures("dataLoad")
-class TestData:
+class TestData(BaseClass):
 
   def test_editProfile(self,dataLoad):
-      print(dataLoad)
-      print("프로필 수정")
+      log = self.getLogger()
+      log.info(dataLoad[0])
+      log.info(dataLoad[2])
+      # print(dataLoad)
+      # print("프로필 수정")
