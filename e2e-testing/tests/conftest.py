94f02d761ae1bcd982a398155f222e27817dd8a8
차민재 <cmjj0824@naver.com>
2025-03-08 17:09:16 +0900

    Chore : addoption과 getoption을 사용한 여러 브라우저 상황에서 테스트 실행하는 코드 작성

--- e2e-testing/tests/conftest.py
+++ e2e-testing/tests/conftest.py
@@ -5,15 +5,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+# pytest addoption으로 여러 브라우저(크롬, 파이어폭스, 사파리..) 띄우기
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store", default="chrome")
+
 @pytest.fixture(scope="class")
 def setup(request):
-    chrome_options = Options()
-    chrome_options.add_argument("--start-maximized") # 크롬창 최대
-    chrome_options.add_experimental_option("detach", True) # 크롬창이 바로 꺼지는 현상을 방지
-
-    driver = webdriver.Chrome(options=chrome_options)
+    browser_name = request.config.getoption("browser_name")
+    selenium_options = Options()
+    selenium_options.add_argument("--start-maximized")
+    selenium_options.add_experimental_option("detach", True) # 크����이 바로 ���지는 현상을 방지
+    
+    # browser_name이 크롬일 때
+    if browser_name == "chrome":
+        driver = webdriver.Chrome(options=selenium_options)
+    # browser_name이 파이어폭스일 때
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox(options=selenium_options)
+    # browser_name이 사파리일 때
+    elif browser_name == "safari":
+        driver = webdriver.Safari(options=selenium_options)
+    
+    # 웹사이트 창 주소
     driver.get("https://rahulshettyacademy.com/angularpractice/")
-    driver.maximize_window()
 
     # driver 모듈 import 시 필요사항
     request.cls.driver = driver
