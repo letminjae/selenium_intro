be21bff51eefd7cecc29fd5039cd79babcfe043f
차민재 <cmjj0824@naver.com>
2025-03-08 16:48:24 +0900

    Chore : angularpractice 사이트 E2E 테스트 pytest로 마이그레이션

--- e2e-testing/tests/conftest.py
+++ e2e-testing/tests/conftest.py
@@ -2,14 +2,21 @@
 import pytest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture()
-def setup():
+@pytest.fixture(scope="class")
+def setup(request):
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized") # 크롬창 최대
     chrome_options.add_experimental_option("detach", True) # 크롬창이 바로 꺼지는 현상을 방지
 
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get("")
+    driver.get("https://rahulshettyacademy.com/angularpractice/")
     driver.maximize_window()
+
+    # driver 모듈 import 시 필요사항
+    request.cls.driver = driver
+
+    yield # test 실행 이후 드라이버를 닫아준다.
+    driver.close()
--- e2e-testing/tests/test_e2e.py
+++ e2e-testing/tests/test_e2e.py
@@ -1,6 +1,43 @@
 # pytest 표준에 맞춰, 셀레니움 코드 작성
+import pytest
+from utilities.base_class import BaseClass
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
-class TestOne:
+class TestOne(BaseClass):
 
   def test_E2E(self):
-    print("E2E Start")
+    self.driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click() # a[href*='shop'] : shop이 포함된 a태그
+
+    # 블랙베리 카드 찾아 장바구니에 추가
+    cards = self.driver.find_elements(By.XPATH, "//div[@class='card h-100']")
+    for card in cards:
+      card_name = card.find_element(By.XPATH, "div/h4/a").text
+      if card_name == "Blackberry":
+        card.find_element(By.XPATH, "div/button").click()
+        break
+      
+    # Checkout 클릭
+    self.driver.find_element(By.CSS_SELECTOR, "a[class*='nav-link btn btn-primary']").click() # a[class*='nav-link btn btn-primary'] : nav-link, btn, btn-primary가 포함된 a태그
+
+    # Checkout 페이지 이동
+    self.driver.find_element(By.CSS_SELECTOR, "button[class*='btn btn-success']").click()
+
+    # Country 입력
+    self.driver.find_element(By.ID, "country").send_keys("ind")
+    wait = WebDriverWait(self.driver, 10)
+    wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "India")))
+    self.driver.find_element(By.LINK_TEXT, "India").click()
+
+    # 약관 동의
+    self.driver.find_element(By.CSS_SELECTOR, "div[class*='checkbox-primary']").click()
+
+    # 구매 완료
+    self.driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+
+    # 완료 박스 확인
+    success_text = self.driver.find_element(By.CLASS_NAME, "alert-success").text
+    assert "Success!" in success_text
+
+
