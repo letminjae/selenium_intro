af930cb5e56143148005d63363c79ffe01d299d0
차민재 <cmjj0824@naver.com>
2025-03-12 20:05:50 +0900

    Chore : 딕셔너리를 사용한 테스트 데이터셋 파라미터화

--- e2e-testing/tests/test_Home.py
+++ e2e-testing/tests/test_Home.py
@@ -2,6 +2,7 @@
 import pytest
 from utilities.base_class import BaseClass
 from page_objects.HomePage import Homepage
+from test_data.HomePageData import HomePageData
 
 class TestHome(BaseClass):
 
@@ -9,10 +10,10 @@ class TestHome(BaseClass):
         # basics/locator.py에 있는 코드 가져와서 리팩토링
         homepage = Homepage(self.driver)
 
-        homepage.enterEmail().send_keys(getData[0])
-        homepage.enterPassword().send_keys(getData[1])
+        homepage.enterEmail().send_keys(getData["email"])
+        homepage.enterPassword().send_keys(getData["password"])
 
-        homepage.enterName().send_keys(getData[2]) 
+        homepage.enterName().send_keys(getData["name"])
         homepage.checkCheckbox().click()
 
         # 드롭다운 - BaseClass에 저장
@@ -22,12 +23,11 @@ class TestHome(BaseClass):
         homepage.submitForm().click()
         message = homepage.getAlertSuccessMessage()
         print(message)
-        
+
         # 여러 데이터를 테스트할 때, 새로고침 필요
         self.driver.refresh()
 
     # 각각의 튜플이 하나의 테스트케이스가 된다.
-    @pytest.fixture(params=[("hello@hello.com","123456","hello"), ("cmj@cmj.com", "qwer1234", "hi")])
+    @pytest.fixture(params=HomePageData.test_homepage_data)
     def getData(self, request):
         return request.param
-
