eeb413f42043adba1e683999e8d3807adb01013c
차민재 <cmjj0824@naver.com>
2025-03-12 20:03:26 +0900

    ADD : test_data/HomePageData.py - Homepage 관련 데이터 저장을 위한 test_data 파일 추가

---
+++ e2e-testing/test_data/HomePageData.py
@@ -0,0 +1,5 @@
+class HomePageData:
+  test_homepage_data = [
+            {"email": "hello@hello.com", "password": "123456", "name": "hello"},
+            {"email": "cmj@cmj.com", "password": "qwer1234", "name": "hi"},
+        ]
