30d869e3e398f10bb85f96ada79d515faa06e056
차민재 <cmjj0824@naver.com>
2025-03-14 21:28:30 +0900

    Chore - 딕셔너리 엑셀 데이터 추가
    추가 처리를 위해 파일에서 딕셔너리로 엑셀 데이터 불러오기

--- e2e-testing/openpyxl/excel_demo.py
+++ e2e-testing/openpyxl/excel_demo.py
@@ -3,6 +3,21 @@ import openpyxl
 excel_file = openpyxl.load_workbook("/Users/chaminjae/Desktop/SQA/python_excel_demo.xlsx")
 
 # 시트 제어
-sheet1 = excel_file.active
-cell = sheet1.cell(row=1, column=2) # Firstname
-print(cell.value) # Firstname
+sheet = excel_file.active
+
+# 시트 값 확인하는 방법
+cell = sheet.cell(row=1, column=2) # 성
+B1 = sheet['B1'].value # 성
+
+# cell 값을 변경함 - 실제 엑셀에선 아직 변경되지 않음.
+sheet.cell(row=2, column=2).value = "Cha" 
+print(sheet.cell(row=2, column=2).value)
+
+print(sheet.max_row) #전체 행
+print(sheet.max_column) # 전체 column 열
+
+# 엑셀 시트 모든 값 출력
+print("모든 값은 아래에 있어요")
+for i in range(1, sheet.max_row+1):
+    for j in range(1, sheet.max_column):
+        print(sheet[i][j].value)
