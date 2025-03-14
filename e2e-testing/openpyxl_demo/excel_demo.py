03969f83f91e673c5785b873e18665f1458228b0
차민재 <cmjj0824@naver.com>
2025-03-14 21:43:36 +0900

    FIX - openpyxl 폴더명 변경
    openpyxl 모듈의 충돌문제로 인해 openpyxl -> openpyxl_demo 폴더로 이름변경

--- e2e-testing/openpyxl_demo/excel_demo.py
+++ e2e-testing/openpyxl_demo/excel_demo.py
@@ -4,6 +4,7 @@ excel_file = openpyxl.load_workbook("/Users/chaminjae/Desktop/SQA/python_excel_d
 
 # 시트 제어
 sheet = excel_file.active
+dict = {}
 
 # 시트 값 확인하는 방법
 cell = sheet.cell(row=1, column=2) # 성
@@ -16,8 +17,11 @@ print(sheet.cell(row=2, column=2).value)
 print(sheet.max_row) #전체 행
 print(sheet.max_column) # 전체 column 열
 
-# 엑셀 시트 모든 값 출력
-print("모든 값은 아래에 있어요")
+# 엑셀 시트 값 출력
 for i in range(1, sheet.max_row+1):
-    for j in range(1, sheet.max_column):
-        print(sheet[i][j].value)
+    for j in range(sheet.max_column):
+        if sheet[i][0].value == "Testcase2":
+            #딕셔너리에 넣기
+            dict[sheet[1][j].value] = sheet[i][j].value
+    
+print(dict)
